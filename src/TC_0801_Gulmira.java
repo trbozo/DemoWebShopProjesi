700cc3cb6ce3adbec51e98c010e7bbad9866cfd0
gu1mira <sidagulova@gmail.com>
2024-03-10 17:03:39 +0200

    Step 3. Clicking on the 14.1-inin Laptop

--- src/TC_0801_Gulmira.java
+++ src/TC_0801_Gulmira.java
@@ -11,10 +11,11 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 public class TC_0801_Gulmira extends BaseDriver {
     @Test
     public void Test801(){
+        //Step 1. Open the web browser and navigate to "Demowebshop.tricentis.com ".
         driver.get("https://demowebshop.tricentis.com/");
         wait.until(ExpectedConditions.urlToBe("https://demowebshop.tricentis.com/"));
-        //MyFunc.Bekle(3);
 
+        //Step 2. If the user session is not open, log in (log in).
         WebElement loginLink= driver.findElement(By.xpath("//*[text()='Log in']"));
         Actions aksiyonlar=new Actions(driver); //// web sayfası aksiyonlara açıldı.
 
@@ -53,6 +54,58 @@ public class TC_0801_Gulmira extends BaseDriver {
                 .click()
                 .build()
                 .perform();
+
+        //Step 3. In the homepage, click "14.1-inin Laptop" under "Notebook" from the "Computers" menu.
+
+        WebElement computers=driver.findElement(By.xpath("(//a[contains(@href,'/computers')])[1]"));
+        MyFunc.Bekle(3);
+
+        new Actions(driver)
+                .moveToElement(computers)
+                .build()
+                .perform();
+        MyFunc.Bekle(3);
+
+        WebElement notebooks= driver.findElement(By.xpath("(//a[contains(@href,'/notebooks')])[1]"));
+        MyFunc.Bekle(3);
+
+        new Actions(driver)
+                .moveToElement(notebooks)
+                .click()
+                .build()
+                .perform();
+
+        WebElement notebook14Inch= driver.findElement(By.xpath("//*[text()='14.1-inch Laptop']"));
+        MyFunc.Bekle(3);
+
+        new Actions(driver)
+                .moveToElement(notebook14Inch)
+                .click()
+                .build()
+                .perform();
+
+
+
+
+
+
+//        4. On the product page, click the "Add to Cart" button to add
+//        the product to the basket.
+//        5. Click the "Shopping Cart" button to navigate to your
+//        basket.
+//        6. Click the "Apply Coupon" button (Use Coupon) (click on
+//        the Coupon Code If you do not enter the code).
+//        7. Verify that there is no coupon code to be used.
+//        8.
+//        9. Verify that there is no Gift Card to be used.
+//        10. Fill the necessary information for the cargo details:
+//        11. Choose a country and state.
+//        12. Accept the conditions.
+//        13. Click the "Checkout" (payment) button to navigate to the
+//        payment page.
+//        14. Fill the payment information and confirm the order.
+//        15 "Your order has been successfully processed!" Verify
+//        that the message is displayed.
 
 
 
