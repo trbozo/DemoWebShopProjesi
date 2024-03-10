67a9aa2a1b7f8f0cba0338710e693681236cba61
gu1mira <sidagulova@gmail.com>
2024-03-10 19:16:25 +0200

    Step 5. Clicking on the button "Shopping card". Step 6. Click on Apply Coupon. Step 7. Click on Add Gift Card

--- src/TC_0801_Gulmira.java
+++ src/TC_0801_Gulmira.java
@@ -1,5 +1,6 @@
 import Utlity.BaseDriver;
 import Utlity.MyFunc;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -82,11 +83,11 @@ public class TC_0801_Gulmira extends BaseDriver {
                 .build()
                 .perform();
 
-        MyFunc.Bekle(3);
-                new Actions(driver).
-                scrollByAmount(0,500).
-                build().
-                perform();
+//        MyFunc.Bekle(3);
+//                new Actions(driver).
+//                scrollByAmount(0,500).
+//                build().
+//                perform();
 
         //Step 4. On the product page, click the "Add to Cart" button to add the product to the basket.
         WebElement addToCart= driver.findElement(By.xpath("//*[@id='add-to-cart-button-31']"));
@@ -98,21 +99,50 @@ public class TC_0801_Gulmira extends BaseDriver {
                 .build()
                 .perform();
 
-//        5. Click the "Shopping Cart" button to navigate to your
-//        basket.
-//        6. Click the "Apply Coupon" button (Use Coupon) (click on
-//        the Coupon Code If you do not enter the code).
-//        7. Verify that there is no coupon code to be used.
-//        8.
-//        9. Verify that there is no Gift Card to be used.
-//        10. Fill the necessary information for the cargo details:
-//        11. Choose a country and state.
-//        12. Accept the conditions.
-//        13. Click the "Checkout" (payment) button to navigate to the
-//        payment page.
-//        14. Fill the payment information and confirm the order.
-//        15 "Your order has been successfully processed!" Verify
-//        that the message is displayed.
+        //Step 5. Click the "Shopping Cart" button to navigate to your basket.  (//*[@class='cart-label'])[1]
+        WebElement shoppingCart= driver.findElement(By.xpath("(//*[@href='/cart'])[1]"));
+        MyFunc.Bekle(3);
+
+        new Actions(driver)
+                .moveToElement(shoppingCart)
+                .click()
+                .build()
+                .perform();
+
+
+        //Step 6. Click the "Apply Coupon" button (Use Coupon) (click on
+        //the Coupon Code If you do not enter the code).
+        WebElement applyCoupon= driver.findElement(By.name("applydiscountcouponcode"));
+        MyFunc.Bekle(3);
+
+        new Actions(driver)
+                .moveToElement(applyCoupon)
+                .click()
+                .build()
+                .perform();
+
+        //Step 7. Verify that there is no coupon code to be used.
+        WebElement couponMessage=driver.findElement(By.xpath("//*[@class='message']"));
+        Assert.assertTrue("There is no message", couponMessage.getText().contains("The coupon code you entered couldn't be applied to your order"));
+        //Step 8. Click the "Add gift card" button (If you do not enter the code).
+        WebElement addGiftCard= driver.findElement(By.name("applygiftcardcouponcode"));
+        MyFunc.Bekle(3);
+
+        new Actions(driver)
+                .moveToElement(addGiftCard)
+                .click()
+                .build()
+                .perform();
+        //Step 9. Verify that there is no Gift Card to be used.
+        WebElement GiftCardMessage=driver.findElement(By.xpath("//*[@class='message']"));
+        MyFunc.Bekle(2);
+        Assert.assertTrue("There is no message", GiftCardMessage.getText().contains("The coupon code you entered couldn't be applied to your order"));
+        //Step 10. Fill the necessary information for the cargo details:
+        //Step 11. Choose a country and state.
+        //Step 12. Accept the conditions.
+        //Step 13. Click the "Checkout" (payment) button to navigate to the payment page.
+        //Step 14. Fill the payment information and confirm the order.
+        //Step 15. "Your order has been successfully processed!" Verify that the message is displayed.
 
 
 
