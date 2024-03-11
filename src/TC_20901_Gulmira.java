054a781568f9ffdd2e1102251bac473a7017d234
gu1mira <sidagulova@gmail.com>
2024-03-11 09:31:34 +0200

    Step 2 is added

--- src/TC_20901_Gulmira.java
+++ src/TC_20901_Gulmira.java
@@ -1,5 +1,11 @@
 import Utlity.BaseDriver;
+import Utlity.MyFunc;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class TC_20901_Gulmira extends BaseDriver {
@@ -9,6 +15,44 @@ public class TC_20901_Gulmira extends BaseDriver {
         driver.get("https://demowebshop.tricentis.com/");
         wait.until(ExpectedConditions.urlToBe("https://demowebshop.tricentis.com/"));
     //Step 2. If the user session is not open, log in (login).
+        WebElement loginLink= driver.findElement(By.xpath("//*[text()='Log in']"));
+        Actions aksiyonlar=new Actions(driver); //// web sayfası aksiyonlara açıldı.
+
+        Action aksiyon= aksiyonlar.moveToElement(loginLink).click().build();
+        MyFunc.Bekle(3);
+        aksiyon.perform();
+
+        WebElement email= driver.findElement(By.xpath("//input[@id='Email']"));
+        MyFunc.Bekle(3);
+
+        new Actions(driver)
+                .moveToElement(email)
+                .click()
+                .sendKeys("ahmet@mail.ru")
+                .build()
+                .perform();
+
+        WebElement password= driver.findElement(By.xpath("//input[@id='Password']"));
+        MyFunc.Bekle(3);
+
+        new Actions(driver)
+                .moveToElement(password)
+                .click()
+                .keyDown(Keys.SHIFT)
+                .sendKeys("a")
+                .keyUp(Keys.SHIFT)
+                .sendKeys("hmet123")
+                .build()
+                .perform();
+
+        WebElement logginButton= driver.findElement(By.xpath("//*[@class='button-1 login-button']"));
+        MyFunc.Bekle(3);
+
+        new Actions(driver)
+                .moveToElement(logginButton)
+                .click()
+                .build()
+                .perform();
 
     //Step 3. Navigate to the "My Account" page by clicking on your username or e-mail address on the homepage.
     //Step 4. View your orders by clicking on the "Orders" (Orders) link.
