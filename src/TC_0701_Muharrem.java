24cf241edd01aefc0c80f669bbf7b88c60c11f9d
muharremyurtsever <muharremyurtsever@gmail.com>
2024-03-13 09:58:53 +0000

    TC_0701_Muharrem.java test case 2.step kısmını yazdım.

--- src/TC_0701_Muharrem.java
+++ src/TC_0701_Muharrem.java
@@ -23,6 +23,48 @@ public class TC_0701_Muharrem extends BaseDriver {
         driver.get("https://demowebshop.tricentis.com/");
         wait.until(ExpectedConditions.urlToBe("https://demowebshop.tricentis.com/"));
 
+        //2. Eğer kullanıcı oturumu açık değilse, oturum açın (giriş yapın).
+        WebElement loginLink= driver.findElement(By.xpath("//*[text()='Log in']"));
+        Actions aksiyonlar=new Actions(driver); //// web sayfası aksiyonlara açıldı.
+
+        Action aksiyon= aksiyonlar.moveToElement(loginLink).click().build();
+        MyFunc.Bekle(3);
+        aksiyon.perform();
+
+        WebElement email= driver.findElement(By.xpath("//input[@id='Email']"));
+        MyFunc.Bekle(3);
+
+        new Actions(driver)
+                .moveToElement(email)
+                .click()
+                .sendKeys("ahmet@mail.ru")
+                .build()
+                .perform();
+
+        WebElement password= driver.findElement(By.xpath("//input[@id='Password']"));
+        MyFunc.Bekle(3);
+
+        new Actions(driver)
+                .moveToElement(password)
+                .click()
+                .keyDown(Keys.SHIFT)
+                .sendKeys("a")
+                .keyUp(Keys.SHIFT)
+                .sendKeys("hmet123")
+                .build()
+                .perform();
+
+        WebElement logginButton= driver.findElement(By.xpath("//*[@class='button-1 login-button']"));
+        MyFunc.Bekle(3);
+
+        new Actions(driver)
+                .moveToElement(logginButton)
+                .click()
+                .build()
+                .perform();
+
+        //3. Anasayfada "Do you like nopCommerce?" sorusuna yanıtlayın.
+
 
     }
 
